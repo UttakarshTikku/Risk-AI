29e86a27189873080f56d5fc9f44b880be0d96a2
UttakarshTikku <utikku1993@gmail.com>
2019-06-11 19:41:59 +1000

    Adding Comments To Beautify

--- previous_turn_checker.py
+++ previous_turn_checker.py
@@ -1,18 +1,28 @@
 import collections
+"""
+This module is used to compare the past two observed sets of world state indicators to
+ascertain how the previous turn was resolved by the opponent. This helps to develop the
+inferential process and create training data for the intent classifier.
+"""
 
+# This method receives old and new world state indicators and returns the resolution
+# of that turn as a resilt. It takes the help from the previous_move_method
 def find_previous_move_results(old_state, new_state, whoami, world):
     if len(old_state) == 0:
         return
-    return_val = []
+    resolution = []
     for old in old_state:
         for key1, val1 in old:
             if key1 == 'AI_NAME':
                 for new in new_state:
                     for key2, val2 in new:
                         if key2 == 'AI_NAME' and val2 == val1:
-                            return_val.append((val1, previous_move(old, new, world)))
-    return return_val
+                            resolution.append((val1, previous_move(old, new, world)))
+    return resolution
 
+
+# This method receives the different keys of the world state indicators as input and 
+# compares them individually
 def previous_move(old, new, world):
     results = get_empty_result_structure(world)
     for key1, val1 in old:
@@ -32,7 +42,8 @@ def previous_move(old, new, world):
                 elif "_BORDER_FORCES" not in key1 and "_FORCES" in key1 and val2 > val1:
                     results["maximised_num_units_in_"+key1.split('_')[0]] = True
     return results
-                
+
+# Creates an empty placeholder for the previous turn resolution                
 def get_empty_result_structure(world):
     results = {}
     results["eliminated_enemy_player"] = False
@@ -44,6 +55,9 @@ def get_empty_result_structure(world):
         results["maximised_num_units_in_"+area] = False
     return results
 
+# This method merges the intended and actual intents before storing them 
+# in the CSV files for future learnings.
+# It is not directly used for decision making.
 def merge_intended_and_actual(ai_name, intent, result, world):
     temp = {}
     temp["AI_NAME"] = ai_name
@@ -51,6 +65,9 @@ def merge_intended_and_actual(ai_name, intent, result, world):
     temp = Merge(temp, Merge(collections.OrderedDict(sorted(intent.items())),collections.OrderedDict(sorted(result.items()))))
     return temp
 
+# This method adapts the actual intent of the agents 
+# in a format suitable for merging with results.
+# It is not directly used for decision making.
 def expand_intent(intent,world):
     temp = {}
     temp["conquer_one_territory"] = intent["conquer_one_territory"]
@@ -68,6 +85,7 @@ def expand_intent(intent,world):
         temp["occupy_"+y] = x
     return temp
 
+# A utility method to merge dictionaries
 def Merge(dict1, dict2): 
     res = {**dict1, **dict2} 
     return res
