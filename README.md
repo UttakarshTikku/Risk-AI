4745b487d85af87fe31e41df2e51363b40394747
UttakarshTikku <utikku1993@gmail.com>
2019-06-11 16:54:30 +1000

    final touches

--- README.md
+++ README.md
@@ -1,44 +1,71 @@
-pyrisk
-======
+________________________________________________________________________
+DECEPTIVE AI IN RISK
+________________________________________________________________________
 
-Intro
------
+INTRODUCTION
+________________________________________________________________________
+This project uses a RISK implementation in Python to study the possibility
+of deceptive artificial intelligence in Stochastic, Turn-Based games.
 
-A simple implementation of a variant of the **Risk** board game for python, designed for playing with AIs.
+The AI agents implemented for this purpose are:
+Intent Recognition Based, Non-Deceptive Agent: ai/test.py
+Intent Recognition Based, Deceptive Agent: ai/deceptive.py
 
-Runs in `python` (2.7 or 3.x) using the `curses` library to display the map (but can be run in pure-console mode).
+The supporting files like
 
-Usage
------
+    configuration.py
+    intent_engine.py
+    previous_turn_checker.py
+    decision_engine.py
 
-``python pyrisk.py FooAI BarAI*2``
+are also used as helpers of the deceptive agents.
 
-Use `--help` to see more detailed options, such as multi-game running. The AI loader assumes that `SomeAI` translates to a class `SomeAI` inheriting from `AI` in `ai/some.py`.
 
-Rules
------
+TO RUN THE GAME (Windows)
+________________________________________________________________________
+The prerequisite is Python version >= 3.5
+First, run setup.bat
+setup.bat will install the dependencies and setup the requisite folder structure. 
+Then the game is runnable.
 
-A minimal version of the **Risk** rules are used:
+Note: If running on a non-windows machine, check running on docker.
 
-- Players start with `35 - 5*players` armies.
-- At the start of the game, territories are chosen one by one until all are claimed, and then the remaining armies may be deployed one at a time to reinforce claimed territories.
-- Each turn, players recieve `3 + territories/3` reinforcements, plus a bonus for any complete continents.
-- A player may make unlimited attacks per round into adjacent territories (including from freshly-conquered territories).
-  - Each combat round, the attacker can attack with up to three armies.
-  - Upon victory, a minimum of that combat round's attackers are moved into the target territory.
-  - The attacker may cease the attack at the end of any combat round.
-  - The defender defends with two armies (unless only one is available).
-  - Each attacking and defending army rolls 1d6. The rolls on each side are ordered and compared. The loser of each complete pair is removed, with the defender winning ties.
-- At the end of each turn, a player may make one free move
-- Victory is achieved by world domination.
+COMMAND LINE ARGS
+________________________________________________________________________
+--nocurses, default=True, description=Disable the ncurses map display
+--nocolor, default=True, description=Display the map without colors
+-l, default=False, description=Write game events to a logfile
+-d, default=0.1, description=Delay in seconds after each action is displayed
+-s, default=None, description=Random number generator seed
+-g, default=1, description=Number of rounds to play
+-w, default=False, description=Pause and wait for a keypress after each action
+players, description=Names of the AI classes to use. May use 'ExampleAI*3' syntax
+--deal", default=False, description="Deal territories rather than letting players choose
+-gs, default=False, description=Grid Search For Intent Classifier
+-rec, default=False, description=Turn Recording CSV Data On Or Off
 
-API
----
+TESTING AND TRAINING FILES
+________________________________________________________________________
+The training and testing files are:
+    training_AI_agents.bat
+    round_robin_tournament.bat
 
-Write a new class extending the `AI` class in `ai/__init__.py`. The methods are documented in that file. At a minimum, the following functions need to be implemented:
+RUNNING ON DOCKER
+________________________________________________________________________
+docker run -ti -v /path/to/code/:/RISK/ --entrypoint /bin/sh ubuntu:18.04
 
-- `initial_placement(self, empty, remaining)`: Return an empty territory if any are still listed in ``empty``, else an existing territory to reinforce.
-- `reinforce(self, available)`: Return a dictionary of territory -> count for the reinforcement step.
-- `attack(self)`: Yield `(from, to, attack_strategy, move_strategy)` tuples for each attack you want to make.
+Then follow the below steps:
+1. apt-get update
+2. apt-get install libssl-dev openssl
+3. wget https://www.python.org/ftp/python/3.5.0/Python-3.5.0.tgz
+4. tar xzvf Python-3.5.0.tgz
+5. cd Python-3.5.0
+6. ./configure
+7. make
+8. make install
 
-The `AI` base class provides objects `game`, `player` and `world` which can be inspected for the current game state. *These are unproxied versions of the main game data structures, so you're trusted not to modify them.*
+NOTE: 
+* If wget is not available, install using `apt install wget`
+* All tests were run on a Windows machine
+* If make is not found on docker, run the command `apt-get install --reinstall make`
+* If while running `./configure`, no acceptable C compiler is found in ${path}, run `apt-get install build-essentials` 
--- round_robin_tournament.bat
+++ round_robin_tournament.bat
@@ -1,4 +1,9 @@
 @echo off
+@break off
+@title Running Round Robin Tournament of All Deceptive Agents 
+@color 0a
+@cls
+
 
 setlocal enabledelayedexpansion 
 set ai_agent[0]=Deceptive0AI
@@ -10,45 +15,45 @@ set ai_agent[5]=Deceptive5AI
 set ai_agent[6]=Deceptive6AI
 set ai_agent[7]=Deceptive7AI
 
-for /l %%a in (0,1,1) do ( 
-    for /l %%b in (%%a,1,1) do (
-        if NOT %%a == %%b start "" /MIN python pyrisk.py !ai_agent[%%a]!:ALPHA !ai_agent[%%b]!:BRAVO -g 10
+for /l %%a in (0,1,6) do ( 
+    for /l %%b in (%%a,1,7) do (
+        if NOT %%a == %%b start "" /MIN python pyrisk.py !ai_agent[%%a]!:ALPHA !ai_agent[%%b]!:BRAVO -g 200 --nocurses
     )
 )
 
 REM UNWRAPPED LOOP FOR PARALLEL EXECUTION
 
-REM start "" /MIN python pyrisk.py !ai_agent[0]!:ALPHA !ai_agent[1]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[0]!:ALPHA !ai_agent[2]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[0]!:ALPHA !ai_agent[3]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[0]!:ALPHA !ai_agent[4]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[0]!:ALPHA !ai_agent[5]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[0]!:ALPHA !ai_agent[6]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[0]!:ALPHA !ai_agent[7]!:BRAVO -g 100
+REM start "" /MIN python pyrisk.py !ai_agent[0]!:ALPHA !ai_agent[1]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[0]!:ALPHA !ai_agent[2]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[0]!:ALPHA !ai_agent[3]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[0]!:ALPHA !ai_agent[4]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[0]!:ALPHA !ai_agent[5]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[0]!:ALPHA !ai_agent[6]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[0]!:ALPHA !ai_agent[7]!:BRAVO -g 200 --nocurses
 
-REM start "" /MIN python pyrisk.py !ai_agent[1]!:ALPHA !ai_agent[2]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[1]!:ALPHA !ai_agent[3]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[1]!:ALPHA !ai_agent[4]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[1]!:ALPHA !ai_agent[5]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[1]!:ALPHA !ai_agent[6]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[1]!:ALPHA !ai_agent[7]!:BRAVO -g 100
+REM start "" /MIN python pyrisk.py !ai_agent[1]!:ALPHA !ai_agent[2]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[1]!:ALPHA !ai_agent[3]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[1]!:ALPHA !ai_agent[4]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[1]!:ALPHA !ai_agent[5]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[1]!:ALPHA !ai_agent[6]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[1]!:ALPHA !ai_agent[7]!:BRAVO -g 200 --nocurses
 
-REM start "" /MIN python pyrisk.py !ai_agent[2]!:ALPHA !ai_agent[3]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[2]!:ALPHA !ai_agent[4]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[2]!:ALPHA !ai_agent[5]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[2]!:ALPHA !ai_agent[6]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[2]!:ALPHA !ai_agent[7]!:BRAVO -g 100
+REM start "" /MIN python pyrisk.py !ai_agent[2]!:ALPHA !ai_agent[3]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[2]!:ALPHA !ai_agent[4]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[2]!:ALPHA !ai_agent[5]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[2]!:ALPHA !ai_agent[6]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[2]!:ALPHA !ai_agent[7]!:BRAVO -g 200 --nocurses
 
-REM start "" /MIN python pyrisk.py !ai_agent[3]!:ALPHA !ai_agent[4]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[3]!:ALPHA !ai_agent[5]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[3]!:ALPHA !ai_agent[6]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[3]!:ALPHA !ai_agent[7]!:BRAVO -g 100
+REM start "" /MIN python pyrisk.py !ai_agent[3]!:ALPHA !ai_agent[4]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[3]!:ALPHA !ai_agent[5]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[3]!:ALPHA !ai_agent[6]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[3]!:ALPHA !ai_agent[7]!:BRAVO -g 200 --nocurses
 
-REM start "" /MIN python pyrisk.py !ai_agent[4]!:ALPHA !ai_agent[5]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[4]!:ALPHA !ai_agent[6]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[4]!:ALPHA !ai_agent[7]!:BRAVO -g 100
+REM start "" /MIN python pyrisk.py !ai_agent[4]!:ALPHA !ai_agent[5]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[4]!:ALPHA !ai_agent[6]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[4]!:ALPHA !ai_agent[7]!:BRAVO -g 200 --nocurses
 
-REM start "" /MIN python pyrisk.py !ai_agent[5]!:ALPHA !ai_agent[6]!:BRAVO -g 100
-REM start "" /MIN python pyrisk.py !ai_agent[5]!:ALPHA !ai_agent[7]!:BRAVO -g 100
+REM start "" /MIN python pyrisk.py !ai_agent[5]!:ALPHA !ai_agent[6]!:BRAVO -g 200 --nocurses
+REM start "" /MIN python pyrisk.py !ai_agent[5]!:ALPHA !ai_agent[7]!:BRAVO -g 200 --nocurses
 
-REM start "" /MIN python pyrisk.py !ai_agent[6]!:ALPHA !ai_agent[7]!:BRAVO -g 100
+REM start "" /MIN python pyrisk.py !ai_agent[6]!:ALPHA !ai_agent[7]!:BRAVO -g 200 --nocurses
